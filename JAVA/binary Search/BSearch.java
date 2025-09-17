42690915a6aa0405bf0ac8a09f21cb945edd32bc
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-09-17 19:13:25 +0530

    new change

--- JAVA/binary Search/BSearch.java
+++ JAVA/binary Search/BSearch.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class main {
+public class BSearch{
     public static void main(String[] args) {
         int[] arr = {-2, -3, 5, 7, 2, 5, 8, 11, 33, 6, 643};
         int target = 2;
