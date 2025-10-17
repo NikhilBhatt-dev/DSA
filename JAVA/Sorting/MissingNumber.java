69bb98ec15d25f0dd0fe4fb5f6b70a66206e7922
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-10-17 07:30:33 +0530

    adding nw file

---
+++ JAVA/Sorting/MissingNumber.java
@@ -0,0 +1,30 @@
+public class MissingNumber {
+
+    public int missingNumber(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // Search for the first number where index != value
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index;
+            }
+        }
+
+        // If all numbers are in place, the missing one is n
+        return arr.length;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
