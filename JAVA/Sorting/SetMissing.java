743278b23203d10919b04916bbe9de60b56a36e0
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-10-17 13:09:07 +0530

    Add

--- JAVA/Sorting/SetMissing.java
+++ JAVA/Sorting/SetMissing.java
@@ -1,25 +1,25 @@
-public class MissingNumber {
-
-    public int missingNumber(int[] arr) {
+public class SetMissing {
+    public int[] findErrorNums(int[] arr) {
         int i = 0;
+
+       
         while (i < arr.length) {
-            int correct = arr[i];
-            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
                 swap(arr, i, correct);
             } else {
                 i++;
             }
         }
 
-        // Search for the first number where index != value
+       
         for (int index = 0; index < arr.length; index++) {
-            if (arr[index] != index) {
-                return index;
+            if (arr[index] != index + 1) {
+                return new int[] {arr[index], index + 1};
             }
         }
 
-        // If all numbers are in place, the missing one is n
-        return arr.length;
+        return new int[] {-1, -1}; 
     }
 
     static void swap(int[] arr, int first, int second) {
