2edbbf06b40380b08bff0924cd2cbca82689c0cf
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-03 21:46:34 +0530

    updated

--- JAVA/Sorting/FindAllDuplicates.java
+++ JAVA/Sorting/FindAllDuplicates.java
@@ -7,7 +7,7 @@ public class FindAllDuplicates {
         while (i < arr.length) {
             int correct = arr[i] - 1;
 
-            if (arr[i] > 0 && arr[i] <= arr.length && arr[i] != arr[correct]) {
+            if (arr[i] != arr[correct]) {
                 swap(arr, i, correct);
             } else {
                 i++;
